dd45de246c94027c27c20a7fdb307f41e8ad7d5b
KevinTranPXL <73336752+KevinTranPXL@users.noreply.github.com>
2022-05-16 12:49:26 +0200

    enige wat betaalmethode nog moet is het icoon boven en onder even hoog zetten

---
+++ betaalmethode.js
@@ -0,0 +1,17 @@
+// WELCOME TO HELLISH PIT OF STATE CODING
+
+
+
+document.getElementById("buttonPressedTop").addEventListener("click" , pingping)
+
+function pingping (){
+    
+    let answer = "pingping"
+    console.log("pingping is ingedrukt")
+}
+
+
+if ( answer == "pingping"){
+     document.querySelector("gaVerderLink")
+
+}
