1309beb65cf928f261ed6b738e53ba99c80022dd
KevinTranPXL <73336752+KevinTranPXL@users.noreply.github.com>
2022-05-17 11:09:17 +0200

    URL CHANGE BUTTON FIXED IK BEN ZO COOL EN SAM OOK (hou wel in gedachte dat ze nu leiden naar fortnite

--- betaalmethode.js
+++ betaalmethode.js
@@ -1,17 +1,34 @@
 // WELCOME TO HELLISH PIT OF STATE CODING
 
+document.getElementById("buttonPressedTop").addEventListener("click", pingping);
+document.getElementById("buttonPressedBot").addEventListener("click", bank);
 
-
-document.getElementById("buttonPressedTop").addEventListener("click" , pingping)
-
-function pingping (){
-    
-    let answer = "pingping"
-    console.log("pingping is ingedrukt")
+function pingping() {
+  let answer = 0;
+  console.log("pingping is ingedrukt");
+  URLChange(answer);
+  //   return answer;
 }
 
+function bank() {
+  let answer = 1;
+  console.log("Bankkaart is ingedrukt");
+  URLChange(answer);
+  //   return answer;
+}
 
-if ( answer == "pingping"){
-     document.querySelector("gaVerderLink")
+function URLChange(answer) {
+  if (answer == 0) {
+    let groeneButton = document.querySelector(".gaVerderLink").href = "https://www.epicgames.com/fortnite/en-US/home";
 
+    // groeneButton = "https://www.epicgames.com/fortnite/en-US/home";
+    // let textNode = document.createTextNode(
+    //   "https://www.epicgames.com/fortnite/en-US/home"
+    // );
+    // groeneButton.appendChild(textNode);
+    console.log("URL CHANGE NAAR PINGPING");
+  } else if (answer == 1) {
+    document.querySelector("gaVerderLink");
+    console.log("URL CHANGE NAAR BANKKAART");
+  }
 }
