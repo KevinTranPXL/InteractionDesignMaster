58e0ac74868aa5871d1d4f7ffa0dc62be295fde4
Sam <12001785@student.pxl.be>
2022-05-27 20:55:42 +0200

    Linken gelegd naar juiste pagina

--- betaal-opties.js
+++ betaal-opties.js
@@ -19,7 +19,7 @@ function bank() {
 
 function URLChange(answer) {
   if (answer == 0) {
-    let groeneButton = document.querySelector(".gaVerderLink").href = "https://www.epicgames.com/fortnite/en-US/home";
+    let groeneButton = document.querySelector(".gaVerderLink").href = "betaling.html";
 
     // groeneButton = "https://www.epicgames.com/fortnite/en-US/home";
     // let textNode = document.createTextNode(
