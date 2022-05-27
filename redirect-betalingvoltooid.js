3999e42cc4c8b02a950d1efe5221dd2eb5ee02fb
KevinTranPXL <73336752+KevinTranPXL@users.noreply.github.com>
2022-05-27 21:05:11 +0200

    timer opgezet bij betaling verwerken

---
+++ redirect-betalingvoltooid.js
@@ -0,0 +1,6 @@
+function redirect() {
+    window.location.replace("betaling-voltooid.html");
+  }
+  
+  setTimeout(redirect, 3000);
+  
--- redirect.js
+++ redirect.js
@@ -1,7 +1,5 @@
-console.log("Sam zei tegen mij los het op :( ")
-
 function redirect() {
   window.location.replace("overzicht-bestelling.html");
-}
+} 
 
 setTimeout(redirect, 2000)
